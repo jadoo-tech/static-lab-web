07803b3f0430668ecddd8357160156e7dc468602
Ronit Nath <ronitnath@berkeley.edu>
2024-11-03 13:44:27 -0800

    new server.js using express

--- server.js
+++ server.js
@@ -1,37 +1,28 @@
-// custom-server.js
 import { handler } from './build/handler.js';
+import express from 'express';
 import http from 'http';
-import { unlinkSync } from 'fs';
+import { Server } from 'socket.io';
 
+const app = express();
+const server = http.createServer(app);
+
+// Add Socket.IO if needed
+const io = new Server(server);
+
+// SvelteKit handler
+app.use(handler);
+
+// Unix socket path
 const socketPath = process.env.PORT;
 
-
-// Remove existing socket file
-try {
-    unlinkSync(socketPath);
-  } catch (err) {
-    if (err.code !== 'ENOENT') {
-      console.error('Error removing socket file:', err);
-      process.exit(1);
-    }
-  }
-
-const server = http.createServer((req, res) => {
-    console.log(`Incoming request: ${req.method} ${req.url}`);
-    console.log('Headers:', req.headers);
-    // Trust the proxy by adjusting the request headers
-    // req.headers['x-forwarded-host'] = req.headers['host'];
-    // req.headers['host'] = req.headers['x-forwarded-host'];
-
-    handler(req, res);
-});
-
-
 server.listen(socketPath, () => {
-  console.log(`Server is listening on Unix socket: ${socketPath}`);
+  console.log(`Server running on Unix socket: ${socketPath}`);
 });
 
-// Handle server errors
-server.on('error', (err) => {
-  console.error('Server error:', err);
+// Cleanup on exit
+process.on('SIGINT', () => {
+  server.close(() => {
+    console.log('Server closed');
+    process.exit(0);
+  });
 });
