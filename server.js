aac907020ed5b4eeb612349c045c9e2efea6eb4f
Ronit Nath <ronitnath@berkeley.edu>
2024-11-03 13:36:19 -0800

    debug added

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const socketPath = process.env.PORT;
 
 
 const server = http.createServer((req, res) => {
+    console.log(`Incoming request: ${req.method} ${req.url}`);
+    console.log('Headers:', req.headers);
     // Trust the proxy by adjusting the request headers
     req.headers['x-forwarded-host'] = req.headers['host'];
     req.headers['host'] = req.headers['x-forwarded-host'];
